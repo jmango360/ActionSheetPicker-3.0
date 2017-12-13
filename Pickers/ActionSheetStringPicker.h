f8cb0bd29aeb7a2fcd8498e290899d7dfa922c17
jmango360 <jmango360@users.noreply.github.com>
2017-12-13 09:33:29 +0700

    Update ActionSheetStringPicker.h
    update display text alignment for  iOS

--- Pickers/ActionSheetStringPicker.h
+++ Pickers/ActionSheetStringPicker.h
@@ -58,5 +58,5 @@ typedef void(^ActionStringCancelBlock)(ActionSheetStringPicker *picker);
 
 @property (nonatomic, copy) ActionStringDoneBlock onActionSheetDone;
 @property (nonatomic, copy) ActionStringCancelBlock onActionSheetCancel;
-
+@property (nonatomic, assign) NSTextAlignment textAlignment;
 @end
